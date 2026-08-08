3aaf795bf7b882f0f52eeb9b184858a75e066700
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-08-08 16:27:15 +0100

    Added a new file to the repo

---
+++ question_1.js
@@ -0,0 +1,14 @@
+const scanner = require("prompt-sync")();
+const grade = Number(scanner("Enter your score: ")); 
+
+if (grade >= 90) { 
+    console.log("A"); 
+} else if (grade >= 80) {
+    console.log("B"); 
+} else if (grade >= 70) { 
+    console.log("C"); 
+} else if (grade >= 60) { 
+    console.log("D"); 
+} else { 
+    console.log('F');   
+}
