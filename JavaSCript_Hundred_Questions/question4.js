504932c4be28186b652e9cc190eb7fce58d42789
Yusufah9 <umaryaruyusuf971@gmail.com>
2026-08-10 14:05:27 +0100

    Remove all files from GitHub repository

--- JavaSCript_Hundred_Questions/question4.js
+++
@@ -1,4 +0,0 @@
-//Write a program that takes three side lengths and returns 'Equilateral' if all sides are equal,
-'//Isosceles' if two are equal, and 'Scalene' if all are different.
-
-
--- JavaSCript_Hundred_Questions/question6.py
+++
@@ -1,3 +0,0 @@
-//Write a program that takes a single digit (0–9) and returns its word form (e.g., three), or
-//invalid for any other input.
-
